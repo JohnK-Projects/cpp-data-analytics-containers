573cf3019639856798e6456834dcfc7fa37087ea
John Kirkham <kirkham.john3550@gmail.com>
2026-04-28 11:12:41 -0600

    Update main.cpp
    Cleaning it up

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,20 @@
 #include "containers.h"
 using namespace std;
 
-// Main Function
+
 int main()
 {
-  // TODO: Part I
-    std::string input = "../../data/MOCK_DATA_CARS.csv";     // DO NOT MODIFY THIS LINE
+    std::string input = "../../data/MOCK_DATA_CARS.csv";   
     std::vector<Car> cars;
-    Load_Data(cars, input); // Load Data
+    Load_Data(cars, input); 
 
     cout <<"Cars size: " << cars.size() << endl;
-  // TODO: Part II
+ 
     Car car = Car_Value_Analytics(cars, MAX);
     cout << "Car MAX value: " << car.value << endl;
     car = Car_Value_Analytics(cars, MIN);
     cout << "Car MIN value: " << car.value << endl;
-  // TODO: Part III
+
     int decade = 1978;
     std::vector<Car> new_cars = Cars_from_decade(cars, decade);
     for (const Car &car : new_cars)
